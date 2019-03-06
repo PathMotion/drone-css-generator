2c33a4b5fdb755c46d8f92920c6673699a758923
Maxime Lebastard <maxime@pathmotion.com>
2019-03-06 16:20:34 +0100

    Fix exit status and conditions

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 set -e
+set +x;
 
-RESULT=$(curl -X POST http://${PLUGIN_PM_FRONTEND_APP}/process-platform-css -H "X-API-Token: ${PLUGIN_API_AUTH_KEY}")
-if [ $RESULT = 'ok' ]; then
-	echo 0
+RESULT=$(curl -v -X POST http://${PLUGIN_PM_FRONTEND_APP}/process-platform-css -H "X-API-Token: ${PLUGIN_API_AUTH_KEY}")
+
+if [ "$RESULT" == "ok" ]; then
+	exit 0
 else
-	echo 1
+	exit 1
 fi
+
